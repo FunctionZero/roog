569f0f2585ce6f948217a58aa080e27f2fc5f097
FunctionZero <the.function.zero@gmail.com>
2013-12-24 21:17:03 +0100

    Created a log/exception system, logs to file.

---
+++ src/system_exception.cpp
@@ -0,0 +1,32 @@
+#include <fstream>
+#include "system_exception.hpp"
+
+namespace System
+{
+    void WriteToFile(std::string argString, std::string filename)
+    {
+        std::ofstream strm(filename.c_str(), std::ios::app);
+
+        strm << argString << '\n';
+        strm.flush();
+        strm.close();
+    }
+
+    void Log(std::string argString)
+    {
+        WriteToFile(argString, "roog.log");
+    }
+
+    void Warning(std::string argString)
+    {
+        argString.insert(0, "Warning: ");
+        WriteToFile(argString, "roog.log");
+    }
+
+    void Error(std::string argString, int argError)
+    {
+        argString.insert(0, "Warning: ");
+        WriteToFile(argString, "roog.log");
+        exit(argError);
+    }
+}
---
+++ src/system_exception.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace System
+{
+    void Log(std::string logstring);
+    void Warning(std::string logstring);
+    void Error(std::string logstring);
+}
